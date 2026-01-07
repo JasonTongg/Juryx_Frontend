a886ad99f7a654a3df96d5fdcb75354c51ea6858
JasonTongg <aboytong123@gmail.com>
2026-01-07 11:57:57 +0700

    setup project

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 export default function Navbar() {
   return (
-    <div className="bg-pink-400 w-full min-h-screen text-6xl flex items-center justify-center">
+    <div className="bg-pink-400 w-full flex items-center justify-center">
       <motion.div
         initial={{ transform: "translateX(-100px)", opacity: 0 }}
         whileInView={{ transform: "translateX(0px)", opacity: 1 }}
--- components/Hero.js
+++ components/Hero.js
@@ -1,24 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
-import {
-  ConnectButton,
-  useConnectModal,
-  useAccountModal,
-  useChainModal,
-} from "@rainbow-me/rainbowkit";
-import { WalletButton } from "@rainbow-me/rainbowkit";
 import { useAccount, useBalance } from "wagmi";
 
 export default function Hero() {
-  const { openConnectModal } = useConnectModal();
-  const { openAccountModal } = useAccountModal();
-  const { openChainModal } = useChainModal();
   const { address, isConnected } = useAccount();
   const { data: balance, isLoading } = useBalance({
     address,
   });
-
-  console.log(balance);
 
   return (
     <div className="bg-green-400 w-full min-h-screen flex items-center justify-center">
@@ -29,14 +17,7 @@ export default function Hero() {
         transition={{ duration: 0.5 }}
         className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center gap-5"
       >
-        {!isConnected ? (
-          <div>
-            <WalletButton wallet="rainbow" />
-            <WalletButton wallet="metamask" />
-            <WalletButton wallet="coinbase" />
-            <WalletButton wallet="walletconnect" />
-          </div>
-        ) : (
+        {isConnected && (
           <div>
             {address && <p>Address: {address}</p>}
             {balance && (
@@ -47,23 +28,6 @@ export default function Hero() {
               </p>
             )}
           </div>
-        )}
-        <ConnectButton />
-
-        {openConnectModal && (
-          <button onClick={openConnectModal} type="button">
-            Open Connect Modal
-          </button>
-        )}
-        {openAccountModal && (
-          <button onClick={openAccountModal} type="button">
-            Open Account Modal
-          </button>
-        )}
-        {openChainModal && (
-          <button onClick={openChainModal} type="button">
-            Open Chain Modal
-          </button>
         )}
       </motion.div>
     </div>
